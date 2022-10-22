3f2ed9415af8930027279f8c7eb6705d9f8ab238
Daniel Einars <contact@dle.dev>
2022-10-22 17:38:17 +0200

    06. Animations
    Signed-off-by: Daniel Einars <contact@dle.dev>

--- src/script.js
+++ src/script.js
@@ -1,67 +1,44 @@
 import './style.css'
 import * as THREE from 'three'
-
+import gsap from "gsap";
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
 
-const axesHelper = new THREE.AxesHelper()
-/**
- * Objects
- */
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({color: 0xff0000})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
 
-
-const group = new THREE.Group()
-scene.add(group)
-
-const cube1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({color: "red"})
-)
-const cube2 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({color: "blue"})
-)
-cube2.position.set(-1.5, 0, 0)
-
-const cube3 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({color: "orangered"})
-)
-
-cube3.position.set(1.5, 0, 0)
-
-
-group.add(cube1)
-group.add(cube2)
-group.add(cube3)
-
-group.rotation.y = 0.5
-group.position.y = -0.5
-/**
- * Sizes
- */
+// Sizes
 const sizes = {
     width: 800,
     height: 600
 }
 
-/**
- * Camera
- */
+// Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.set(0, 0, 3)
+camera.position.z = 3
 scene.add(camera)
-scene.add(axesHelper)
 
-
-/**
- * Renderer
- */
+// Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+const clock = new THREE.Clock()
+
+gsap.to(mesh.position, {duration: 1, delay: 1, x: 2})
+gsap.to(mesh.position, {duration: 1, delay: 2, x: 0})
+// Animations
+const tick = () => {
+    // render
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(tick)
+}
+
+tick()
